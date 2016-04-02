da4a4bd02b02ecd618223aa45731fc61127e487e
Kevin Stricker <k.stricker@gmail.com>
2016-04-01 19:08:48 -0600

    Update robinautovoter.user.js
    Buttons?

--- robinautovoter.user.js
+++ robinautovoter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Autovoter
 // @namespace    http://jerl.im
-// @version      3.0
+// @version      3.1
 // @description  Autovotes via text on /r/robin
 // @author       /u/mootinator
 // @match        https://www.reddit.com/robin*
@@ -10,10 +10,23 @@
 /* jshint -W097 */
 'use strict';
 
+
 function sendMessage(message){
     $("#robinSendMessage > input[type='text']").val(message);
     $("#robinSendMessage > input[type='submit']").click();
 }
+
+function updateStatus() {
+    $('#doIStayOrDoIGrowNow').text('Autovoting ' + robinCommand);
+}
+
+var robinCommand = "grow";
+
+$(function() {
+    $('body').prepend('<div id="doIStayOrDoIGrowNow">Autovoting " + robinCommand + "</div><button id="stay">Stay</button><button id="grow">Grow</button></div>');
+    $('#grow').click(function() { robinCommand = "grow"; updateStatus(); });
+    $('#stay').click(function() { robinCommnad = "stay"; updateStatus(); });
+});
 
 setTimeout(function(){
     var participants = $(".robin-room-participant").length;
@@ -21,8 +34,8 @@ setTimeout(function(){
     if (participants == 200) partiText = 200 + " " + $(".robin-user-list-overflow-indicator").text();
     else partiText = participants;
     
-    sendMessage("/vote stay");
-    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 3.0] Autovoted stay!");
+    sendMessage("/vote " + command);
+    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 3.0] Autovoted " + robinCommand + "!");
     setTimeout(function(){
         window.location.reload();
     }, 300000);
